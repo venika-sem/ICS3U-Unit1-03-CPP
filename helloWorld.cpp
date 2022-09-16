2a7e007fe3acc0e52f9517c5ca2f38d1d0312721
Venika Sem <venika.sem@mths.ca>
2022-09-16 14:52:53 +0000

    changed helloWorld.cpp

---
+++ helloWorld.cpp
@@ -0,0 +1,13 @@
+// Copyright (c) 2022 Venika Sem All rights reserved
+//
+// Created by: Venika Sem
+// Created on: Sept 2022
+// This is the "Hello, World!" program, with proper style
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!\n";
+
+    std::cout << "\nDone.";
+}
